f122b0d5fad6a7ff9113b3e14d2ddc15a8829a65
Wojtek Galaj <wojtek@TUICP6TT74WFK.local>
2022-10-30 08:45:47 +0100

    lvim remove deprecated plugin

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -60,7 +60,6 @@ lvim.builtin.which_key.mappings["b"] = {
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = false
 lvim.builtin.terminal.active = true
 lvim.builtin.terminal.open_mapping = "<c-t>"
 lvim.builtin.nvimtree.setup.view.side = "right"
