fe1af6d46424aade84b566b046a77d57d2bb5c48
Wojtek Galaj <wojtek.galaj@tui.com>
2024-03-08 23:22:40 +0100

    point dap at the adapter

--- nvim/.config/nvim/after/plugin/dap.lua
+++ nvim/.config/nvim/after/plugin/dap.lua
@@ -2,7 +2,7 @@ local dap = require("dap")
 
 dap.adapters.lldb = {
 	type = "executable",
-	command = "/usr/bin/lldb-vscode", -- adjust as needed
+	command = "/opt/homebrew/bin/lldb-vscode", -- adjust as needed
 	name = "lldb",
 }
 
