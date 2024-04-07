a8e654c73d6095be93b6e3e5310fa7fed2e80b4b
Wojtek Galaj <wojtek.galaj@hey.com>
2024-04-07 12:52:13 +0200

    toggle venter when entering markdown files

--- nvim/.config/nvim/after/plugin/venter.lua
+++ nvim/.config/nvim/after/plugin/venter.lua
@@ -2,13 +2,20 @@ local wk = require("which-key")
 
 wk.register({
 	["<leader>"] = {
-		c = {
-			name = "[C]enter",
-			s = { "<cmd>VenterToggle<cr>", "[C]enter [S]creen" },
+		t = {
+			name = "[T]oggle",
+			s = { "<cmd>VenterToggle<cr>", "[T]oggle center [S]creen" },
 		},
 	},
 }, {
 	mode = "n",
 	silent = true,
 	noremap = true,
+})
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+	pattern = "*.md",
+	callback = function()
+		vim.api.nvim_command("Venter")
+	end,
 })
