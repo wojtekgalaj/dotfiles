591ea375cf37876eda34d23560f0a4697604c4fe
Wojtek <wojtek.galaj@hey.com>
2024-04-12 10:55:26 +0200

    nvim: toggle venter on off md files

--- nvim/.config/nvim/after/plugin/venter.lua
+++ nvim/.config/nvim/after/plugin/venter.lua
@@ -13,9 +13,22 @@ wk.register({
 	noremap = true,
 })
 
+local function toggle_center_screen()
+	vim.api.nvim_command("VenterToggle")
+end
+
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
 	pattern = "*.md",
+	desc = "Toggle Venter when entering a markdown buffer",
 	callback = function()
-		vim.api.nvim_command("Venter")
+		pcall(toggle_center_screen)
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
+	pattern = { "*.md" },
+	desc = "Toggle Venter when leaving a markdown buffer",
+	callback = function()
+		pcall(toggle_center_screen)
 	end,
 })
