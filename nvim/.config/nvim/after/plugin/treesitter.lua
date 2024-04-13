50f03918b08d5df34a3389fd6802724e7329d9d9
Wojtek <wojtek.galaj@hey.com>
2024-04-13 13:53:06 +0200

    nvim: ensure ts bash installed

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -3,6 +3,7 @@
 require("nvim-treesitter.configs").setup({
 	-- Add languages to be installed here that you want installed for treesitter
 	ensure_installed = {
+		"bash",
 		"http",
 		"go",
 		"lua",
