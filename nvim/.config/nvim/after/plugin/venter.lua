ab0ac958f65954394cb4ebe6fbd16edfa9da8162
Wojtek <wojtek.galaj@hey.com>
2024-04-13 13:54:46 +0200

    nvim: smarter venter setup for md files

--- nvim/.config/nvim/after/plugin/venter.lua
+++ nvim/.config/nvim/after/plugin/venter.lua
@@ -13,22 +13,33 @@ wk.register({
 	noremap = true,
 })
 
-local function toggle_center_screen()
-	vim.api.nvim_command("VenterToggle")
+local function silent_venter_open()
+	local window_width = vim.api.nvim_win_get_width(0)
+	if window_width > 120 then
+		vim.api.nvim_command("Venter")
+	end
 end
+
+local function silent_venter_close()
+	vim.api.nvim_command("VenterClose")
+end
+
+local venter_autogroup = vim.api.nvim_create_augroup("venter_autogroup", { clear = true })
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
 	pattern = "*.md",
 	desc = "Toggle Venter when entering a markdown buffer",
 	callback = function()
-		pcall(toggle_center_screen)
+		pcall(silent_venter_open)
 	end,
+	group = venter_autogroup,
 })
 
 vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
 	pattern = { "*.md" },
 	desc = "Toggle Venter when leaving a markdown buffer",
 	callback = function()
-		pcall(toggle_center_screen)
+		pcall(silent_venter_close)
 	end,
+	group = venter_autogroup,
 })
