73b3e75683be31decfb0f9db981903bae90a2b3c
Wojtek <wojtek.galaj@hey.com>
2024-04-27 13:09:09 +0200

    nvim: insert cmp completion

--- nvim/.config/nvim/after/plugin/cmp.lua
+++ nvim/.config/nvim/after/plugin/cmp.lua
@@ -2,15 +2,12 @@
 local cmp = require("cmp")
 
 cmp.setup({
-	snippet = {
-		expand = function() end,
-	},
 	mapping = cmp.mapping.preset.insert({
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-a>"] = cmp.get_entries(),
 		["<C-y>"] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
+			behavior = cmp.ConfirmBehavior.Insert,
 			select = true,
 		}),
 	}),
