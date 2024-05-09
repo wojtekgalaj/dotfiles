a52895c607fe61daee64594252a73217815a7c26
Wojtek <wojtek.galaj@hey.com>
2024-05-09 12:59:05 +0200

    cleanup

--- nvim/.config/nvim/after/plugin/rest.lua
+++
@@ -1,7 +0,0 @@
--- local rest = require("rest-nvim")
---
--- rest.setup({
--- 	result_split_in_place = true,
--- 	result_split_horizontal = true,
--- })
--- vim.api.nvim_set_keymap("n", "<leader>rs", "<Plug>RestNvim", { noremap = true, silent = true })
