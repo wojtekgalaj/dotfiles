216dde5161a1dbddbf605234248c979431fc9eb2
Wojtek <wojtek.galaj@hey.com>
2024-05-21 13:02:09 +0200

    nvim: add graphql lsp server

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -44,8 +44,18 @@ require("mason").setup()
 -- Enable the following language servers
 -- Feel free to add/remove any LSPs that you want here. They will automatically be installed
 
-local servers =
-	{ "lua_ls", "gopls", "svelte", "tailwindcss", "tsserver", "eslint", "yamlls", "cssls", "emmet_language_server" }
+local servers = {
+	"lua_ls",
+	"gopls",
+	"svelte",
+	"tailwindcss",
+	"tsserver",
+	"eslint",
+	"yamlls",
+	"cssls",
+	"emmet_language_server",
+	"graphql",
+}
 
 -- Ensure the servers above are installed
 require("mason-lspconfig").setup({
