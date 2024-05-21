518fd347ddac1b598a6783ce81340e418b6ba250
Wojtek <wojtek.galaj@hey.com>
2024-05-21 13:01:47 +0200

    nvim: fix oil with navigation

--- nvim/.config/nvim/after/plugin/oil.lua
+++ nvim/.config/nvim/after/plugin/oil.lua
@@ -1,5 +1,18 @@
 local oil = require("oil")
 
-oil.setup()
+oil.setup({
+	use_default_keymaps = false,
+	keymaps = {
+		["g?"] = "actions.show_help",
+		["<CR>"] = "actions.select",
+		["<C-p>"] = "actions.preview",
+		["<C-c>"] = "actions.close",
+		["-"] = "actions.parent",
+		["g."] = "actions.toggle_hidden",
+	},
+	view_options = {
+		show_hidden = true,
+	},
+})
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { silent = true, desc = "Open parent directory" })
