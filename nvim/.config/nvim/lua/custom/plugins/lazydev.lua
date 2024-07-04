95faa2942b14299454365676d1f3926547aabc57
Wojtek <wojtek.galaj@hey.com>
2024-07-04 12:39:24 +0200

    nvim: plugin: lazydev

---
+++ nvim/.config/nvim/lua/custom/plugins/lazydev.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- Library paths can be absolute
+        "~/projects/my-awesome-lib",
+        -- Or relative, which means they will be resolved from the plugin dir.
+        "lazy.nvim",
+        "luvit-meta/library",
+        -- It can also be a table with trigger words / mods
+        -- Only load luvit types when the `vim.uv` word is found
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+        -- always load the LazyVim library
+        "LazyVim",
+        -- Only load the lazyvim library when the `LazyVim` global is found
+        { path = "LazyVim", words = { "LazyVim" } },
+        -- Load the wezterm types when the `wezterm` module is required
+        -- Needs `justinsgithub/wezterm-types` to be installed
+        { path = "wezterm-types", mods = { "wezterm" } },
+      },
+    },
+  },
+}
