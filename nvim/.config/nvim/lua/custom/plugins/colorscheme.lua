f584acb484c758f6b40f8a33d07a78b5c5d40f18
Wojtek <wojtek.galaj@hey.com>
2024-07-04 18:14:13 +0200

    nvim: catppuccin telescope integration

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -3,6 +3,12 @@ return {
     "catppuccin/nvim",
     lazy = false,
     priority = 1000,
+    integrations = {
+      telescope = {
+        enabled = true,
+        style = "nvchad",
+      },
+    },
     config = function()
       vim.o.termguicolors = true
       vim.cmd.colorscheme "catppuccin"
