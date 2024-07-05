507d223fa0527d9468ae279ff26797b3db5a1401
Wojtek <wojtek.galaj@hey.com>
2024-07-05 11:51:55 +0200

    nvim: use mini.doc not neogen

--- nvim/.config/nvim/lua/custom/plugins/mini.lua
+++ nvim/.config/nvim/lua/custom/plugins/mini.lua
@@ -4,6 +4,7 @@ return {
     config = function()
       require("mini.ai").setup {}
       require("mini.surround").setup {}
+      require("mini.doc").setup {}
       require("mini.comment").setup {}
       require("mini.statusline").setup {}
       require("mini.diff").setup {}
