e56f986fd7eb398479955a2e0a3133eef19b8c89
Wojtek <wojtek.galaj@hey.com>
2024-07-05 19:10:58 +0200

    nvim: minmi jump and pairs

--- nvim/.config/nvim/lua/custom/plugins/mini.lua
+++ nvim/.config/nvim/lua/custom/plugins/mini.lua
@@ -4,10 +4,12 @@ return {
     config = function()
       require("mini.ai").setup {}
       require("mini.surround").setup {}
+      require("mini.jump2d").setup()
       require("mini.doc").setup {}
       require("mini.comment").setup {}
       require("mini.statusline").setup {}
       require("mini.diff").setup {}
+      require("mini.pairs").setup {}
     end,
   },
 }
