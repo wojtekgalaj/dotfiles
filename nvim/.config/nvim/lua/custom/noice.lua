ab7a00366efc5f8f756c8698cb384a3ff15f43ea
Wojtek <wojtek.galaj@hey.com>
2024-07-06 12:05:42 +0200

    nvim: noice tweaks

--- nvim/.config/nvim/lua/custom/noice.lua
+++ nvim/.config/nvim/lua/custom/noice.lua
@@ -3,8 +3,24 @@ require("noice").setup {
     progress = {
       enabled = false,
     },
+    hover = {
+      silent = true,
+    },
   },
   presets = {
     long_message_to_split = true,
+    lsp_doc_border = true,
   },
 }
+
+vim.keymap.set({ "n", "i", "s" }, "<c-f>", function()
+  if not require("noice.lsp").scroll(4) then
+    return "<c-f>"
+  end
+end, { silent = true, expr = true })
+
+vim.keymap.set({ "n", "i", "s" }, "<c-b>", function()
+  if not require("noice.lsp").scroll(-4) then
+    return "<c-b>"
+  end
+end, { silent = true, expr = true })
