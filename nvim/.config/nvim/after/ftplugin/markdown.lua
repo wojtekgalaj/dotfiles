f9b0225d446e64d44185c0dae76a823a9cfdce3a
Wojtek <wojtek.galaj@hey.com>
2024-11-27 12:37:38 +0100

    move nvim shennenigans

---
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,2 @@
+local opt = vim.o
+opt.wrap = true
---
+++ nvim/.config/nvim/after/ftplugin/typescript.lua
@@ -0,0 +1,6 @@
+local wk = require "which-key"
+-- TSToolsFileReferences
+-- TSToolsRenameFile
+wk.add {
+  { "gd", "<cmd>TSToolsGoToSourceDefinition<cr>", desc = "Go to source [d]efinition" },
+}
