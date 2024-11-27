61a5ac7af22b558628426b1f7572407c4e4cfb0a
Wojtek <wojtek.galaj@hey.com>
2024-11-27 12:42:28 +0100

    nvim: new colors

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,17 +1,48 @@
 return {
+  -- START CATPPUCCIN
+  -- {
+  --   "catppuccin/nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   integrations = {
+  --     telescope = {
+  --       enabled = true,
+  --       style = "nvchad",
+  --     },
+  --   },
+  --   config = function()
+  --     vim.o.termguicolors = true
+  --     vim.cmd.colorscheme "catppuccin"
+  --   end,
+  -- },
+  -- END CATPPUCCIN
+
   {
-    "catppuccin/nvim",
+    "slugbyte/lackluster.nvim",
     lazy = false,
     priority = 1000,
-    integrations = {
-      telescope = {
-        enabled = true,
-        style = "nvchad",
-      },
-    },
-    config = function()
-      vim.o.termguicolors = true
-      vim.cmd.colorscheme "catppuccin"
+    init = function()
+      -- vim.cmd.colorscheme "lackluster"
+      vim.cmd.colorscheme "lackluster-hack" -- my favorite
+      -- vim.cmd.colorscheme("lackluster-mint")
     end,
   },
+
+  -- {
+  --   "olivercederborg/poimandres.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require("poimandres").setup {
+  --       -- leave this setup function empty for default config
+  --       -- or refer to the configuration section
+  --       -- for configuration options
+  --     }
+  --   end,
+  --
+  --   -- optionally set the colorscheme within lazy config
+  --   init = function()
+  --     vim.cmd "colorscheme poimandres"
+  --   end,
+  -- },
 }
