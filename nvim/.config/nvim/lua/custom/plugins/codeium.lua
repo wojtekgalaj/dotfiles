74f242a1d9b7bc5bbd53afa1347bb4cb30ab0b6c
Wojtek <wojtek.galaj@hey.com>
2024-11-30 11:48:28 +0100

    nvim: use codeium vim, not nvim

--- nvim/.config/nvim/lua/custom/plugins/codeium.lua
+++ nvim/.config/nvim/lua/custom/plugins/codeium.lua
@@ -1,21 +1,41 @@
+-- return {
+--   {
+--     "Exafunction/codeium.nvim",
+--     dependencies = {
+--       "nvim-lua/plenary.nvim",
+--       "hrsh7th/nvim-cmp",
+--     },
+--     config = function()
+--       require("codeium").setup {
+--         virtual_text = { enable = true },
+--       }
+--
+--       local wk = require "which-key"
+--       wk.add {
+--         mode = "n",
+--         { "<leader>c", group = "[c]ode" },
+--         { "<leader>cc", "<cmd>Codeium Chat<cr>", desc = "Codeium [c]hat" },
+--       }
+--     end,
+--   },
+-- }-- Remove the `use` here if you're using folke/lazy.nvim.
 return {
   {
-    "Exafunction/codeium.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
-    },
+    "Exafunction/codeium.vim",
     config = function()
-      require("codeium").setup {
-        virtual_text = { enable = true },
-      }
-
-      local wk = require "which-key"
-      wk.add {
-        mode = "n",
-        { "<leader>c", group = "[c]ode" },
-        { "<leader>cc", "<cmd>Codeium Chat<cr>", desc = "Codeium [c]hat" },
-      }
+      -- Change '<C-g>' here to any keycode you like.
+      vim.keymap.set("i", "<c-y>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true, silent = true })
+      vim.keymap.set("i", "<c-n>", function()
+        return vim.fn["codeium#CycleCompletions"](1)
+      end, { expr = true, silent = true })
+      vim.keymap.set("i", "<c-p>", function()
+        return vim.fn["codeium#CycleCompletions"](-1)
+      end, { expr = true, silent = true })
+      vim.keymap.set("i", "<c-k>", function()
+        return vim.fn["codeium#Clear"]()
+      end, { expr = true, silent = true })
     end,
   },
 }
