034f713f5e0f1e99646f0930857bd647b32a821c
Wojtek <wojtek.galaj@hey.com>
2024-11-30 11:48:58 +0100

    fix smear to help files

--- nvim/.config/nvim/lua/custom/plugins/smear_cursor.lua
+++ nvim/.config/nvim/lua/custom/plugins/smear_cursor.lua
@@ -1,6 +1,16 @@
 return {
   {
     "sphamba/smear-cursor.nvim",
-    opts = {},
+    opts = {
+      -- Smear cursor when switching buffers or windows.
+      smear_between_buffers = false,
+
+      -- Smear cursor when moving within line or to neighbor lines.
+      smear_between_neighbor_lines = true,
+
+      -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
+      -- Smears will blend better on all backgrounds.
+      legacy_computing_symbols_support = true,
+    },
   },
 }
