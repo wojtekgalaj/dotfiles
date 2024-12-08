ba7f25b383a0d68682c67a36fc3257072c2a1a87
Wojtek <wojtek.galaj@hey.com>
2024-12-08 12:13:04 +0100

    nvim: tweaks

--- nvim/.config/nvim/lua/custom/plugins/telescope.lua
+++ nvim/.config/nvim/lua/custom/plugins/telescope.lua
@@ -4,7 +4,11 @@ return {
     dependencies = {
       { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
       { "nvim-telescope/telescope-smart-history.nvim" },
+      { "nvim-telescope/telescope-ui-select.nvim" },
+      { "nvim-telescope/telescope-frecency.nvim" },
       { "nvim-lua/plenary.nvim" },
+      { "nvim-lua/popup.nvim" },
+      { "nvim-telescope/telescope-media-files.nvim" },
       { "rcarriga/nvim-notify" },
     },
     config = function()
