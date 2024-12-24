9c67bdd0bc1fcf9e1d615d26f7f2a13217fffa35
Wojtek <wojtek.galaj@hey.com>
2024-12-24 16:32:16 +0100

    nvim: add explicit mini surround keymaps

--- nvim/.config/nvim/lua/custom/plugins/mini.lua
+++ nvim/.config/nvim/lua/custom/plugins/mini.lua
@@ -3,7 +3,18 @@ return {
     "echasnovski/mini.nvim",
     config = function()
       require("mini.ai").setup {}
-      require("mini.surround").setup {}
+      require("mini.surround").setup {
+        mappings = {
+          add = "sa",
+          delete = "sd",
+          find = "sf",
+          find_left = "sF",
+          highlight = "sh",
+          replace = "sr",
+          update_n_lines = "sn",
+        },
+      }
+      require("mini.jump").setup()
       require("mini.jump2d").setup()
       require("mini.icons").setup {}
       require("mini.indentscope").setup {
