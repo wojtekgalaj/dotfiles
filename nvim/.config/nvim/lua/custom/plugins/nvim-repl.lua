24b4d27c59a613ea3b01b55d13a7370a4bfc5366
Wojtek <wojtek.galaj@hey.com>
2024-12-24 16:30:29 +0100

    nvim: add repl settings

--- nvim/.config/nvim/lua/custom/plugins/nvim-repl.lua
+++ nvim/.config/nvim/lua/custom/plugins/nvim-repl.lua
@@ -5,6 +5,13 @@ return {
       vim.g["repl_filetype_commands"] = {
         javascript = "node",
         svelte = "node",
+        lua = "lua",
+        typescript = "node",
+        typescriptreact = "node",
+        go = "go run",
+        rust = "cargo run",
+        python = "python3",
+        sh = "bash",
       }
     end,
   },
