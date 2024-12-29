e3845748192806f9cbc5ac8d9110c79e2a13b885
Wojtek <wojtek.galaj@hey.com>
2024-12-29 14:57:01 +0100

    nvim: add ai info to statusline

--- nvim/.config/nvim/lua/custom/statusline.lua
+++ nvim/.config/nvim/lua/custom/statusline.lua
@@ -1,4 +1,5 @@
 local M = {}
+local codeium = require "codeium"
 
 function M.setup(opts)
   require("mini.statusline").setup {
@@ -14,13 +15,13 @@ function M.setup(opts)
         local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 120 }
         local location = MiniStatusline.section_location { trunc_width = 75 }
         local search = MiniStatusline.section_searchcount { trunc_width = 75 }
-
         return MiniStatusline.combine_groups {
           { hl = mode_hl, strings = { mode } },
           { hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics, lsp } },
           "%<", -- Mark general truncate point
           { hl = "MiniStatuslineFilename", strings = { filename } },
           "%=", -- End left alignment
+          { strings = { "󱚟:" .. require("codeium.virtual_text").status_string() } },
           { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
           { hl = mode_hl, strings = { search, location } },
         }
