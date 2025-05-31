bd23860e021b5e870ed6f113920eec67fe1b274e
Wojtek <wojtek.galaj@hey.com>
2025-05-31 10:59:31 +0200

    nvim: neotest setup cleanup

--- nvim/.config/nvim/lua/custom/plugins/neotest.lua
+++ nvim/.config/nvim/lua/custom/plugins/neotest.lua
@@ -9,41 +9,10 @@ return {
     },
     config = function()
       require("neotest").setup {
-        discovery = {
-          enabled = true,
-          extension = { "ts", "tsx", "js", "jsx", "json", "css", "scss", "html" },
-        },
         adapters = {
           require "neotest-jest" {
-            discovery = {
-              enabled = true,
-            },
-            jestCommand = "yarn test --",
-            jestConfigFile = function(file)
-              if string.find(file, "/packages/") then
-                return string.match(file, "(.-/[^/]+/)src") .. "jest.config.ts"
-              end
-
-              return vim.fn.getcwd() .. "/jest.config.ts"
-            end,
-            env = { CI = true },
-            cwd = function(path)
-              return vim.fn.getcwd()
-            end,
-          },
-          require "neotest-jest" {
-            discovery = {
-              enabled = true,
-            },
-            jestCommand = "backstage-cli test --",
-            jestConfigFile = function(file)
-              -- No need to specify a jest config file as backstage-cli handles it
-              return nil
-            end,
-            env = { CI = true },
-            cwd = function(path)
-              return vim.fn.getcwd()
-            end,
+            jestCommand = "npm test --",
+            jestConfigFile = "jest.config.js",
           },
         },
       }
