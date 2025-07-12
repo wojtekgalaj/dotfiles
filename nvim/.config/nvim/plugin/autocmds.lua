38cb93f56b09846987a4fcae5bb2053c99799232
Wojtek <wojtek.galaj@hey.com>
2025-07-12 17:33:22 +0200

    nvim: tweak md and obsidian autocmds

--- nvim/.config/nvim/plugin/autocmds.lua
+++ nvim/.config/nvim/plugin/autocmds.lua
@@ -17,6 +17,43 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
   end,
 })
 
+--- check if the `filepath` is in the `target_folder`
+--- @param filepath string # Buffer name
+--- @param target_folder string # name of the folder to check against
+--- @returns boolean
+local function is_in_target_folder(filepath, target_folder)
+  local Path = require "plenary.path"
+  local path = Path:new(filepath):parent()
+  while path.filename ~= "/" do
+    local basename = vim.fn.fnamemodify(path.filename, ":t")
+    if path:make_relative "/" == target_folder or basename == target_folder then
+      return true
+    end
+    path = path:parent()
+  end
+  return false
+end
+
+--- check if `filepath` is in my obsidian folder
+--- @param filepath string Buffer name
+--- @returns boolean
+local function is_in_obsidian_vault(filepath)
+  return is_in_target_folder(filepath, "wojteks_vault")
+end
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  pattern = { "*.md" },
+  callback = function()
+    vim.opt_local.wrap = true
+    -- check if in the obsidian valut
+    -- if yes, disable render markdown
+    local bufname = vim.api.nvim_buf_get_name(0)
+    if bufname ~= "" and is_in_obsidian_vault(bufname) then
+      require("render-markdown").buf_disable()
+    end
+  end,
+})
+
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "*.sh", "bash", "*.zsh", "*.fish", "*.ksh", "sketchybarrc" },
   callback = function()
