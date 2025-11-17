23cddab00d67d63fe537c0f0df6ef779a5354102
Wojtek <wojtek.galaj@hey.com>
2025-11-17 16:21:14 +0100

    nvim: add mcphub for avante

--- nvim/.config/nvim/lua/custom/plugins/avante.lua
+++ nvim/.config/nvim/lua/custom/plugins/avante.lua
@@ -15,6 +15,15 @@ return {
       instructions_file = "agents.md",
       -- for example
       provider = "copilot",
+      system_prompt = function()
+        local hub = require("mcphub").get_hub_instance()
+        return hub and hub:get_active_servers_prompt() or ""
+      end,
+      custom_tools = function()
+        return {
+          require("mcphub.extensions.avante").mcp_tool(),
+        }
+      end,
     },
     dependencies = {
       "nvim-lua/plenary.nvim",
