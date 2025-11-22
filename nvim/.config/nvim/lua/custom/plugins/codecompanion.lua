19ecdee97e7efe41d0030dc55f8f98838a648c6a
Wojtek <wojtek.galaj@hey.com>
2025-11-22 11:48:04 +0100

    nvim: avante again

--- nvim/.config/nvim/lua/custom/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/custom/plugins/codecompanion.lua
@@ -2,7 +2,7 @@ return {
   {
     "olimorris/codecompanion.nvim",
     config = true,
-    enabled = false,
+    enabled = true,
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
