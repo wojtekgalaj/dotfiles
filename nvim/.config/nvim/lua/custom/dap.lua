58413eb589af8f38ce91ee110cd847b9d83bf04f
Wojtek <wojtek.galaj@hey.com>
2025-12-08 07:49:09 +0100

    nvim:da

--- nvim/.config/nvim/lua/custom/dap.lua
+++ nvim/.config/nvim/lua/custom/dap.lua
@@ -8,6 +8,47 @@ require("mason-nvim-dap").setup {
   handlers = {},
 }
 
+dap.adapters["pwa-node"] = {
+  type = "server",
+  host = "localhost",
+  port = "${port}",
+  executable = {
+    command = "js-debug-adapter",
+    args = { "${port}" },
+  },
+}
+
+for _, language in ipairs { "typescript", "javascript", "typescriptreact", "javascriptreact" } do
+  dap.configurations[language] = {
+    {
+      type = "pwa-node",
+      request = "launch",
+      name = "Debug Jest Tests",
+      runtimeExecutable = "node",
+      runtimeArgs = { "./node_modules/.bin/jest", "--runInBand", "--no-coverage", "--no-cache", "${file}" },
+      rootPath = "${workspaceFolder}",
+      cwd = "${workspaceFolder}",
+      console = "integratedTerminal",
+      internalConsoleOptions = "neverOpen",
+      sourceMaps = true,
+      resolveSourceMapLocations = { "${workspaceFolder}/**", "!**/node_modules/**" },
+    },
+    {
+      type = "pwa-node",
+      request = "launch",
+      name = "Debug Vitest Tests",
+      runtimeExecutable = "node",
+      runtimeArgs = { "./node_modules/.bin/vitest", "--run", "${file}" },
+      rootPath = "${workspaceFolder}",
+      cwd = "${workspaceFolder}",
+      console = "integratedTerminal",
+      internalConsoleOptions = "neverOpen",
+      sourceMaps = true,
+      resolveSourceMapLocations = { "${workspaceFolder}/**", "!**/node_modules/**" },
+    },
+  }
+end
+
 vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "", linehl = "", numhl = "" })
 vim.fn.sign_define("DapBreakpointRejected", { text = "", texthl = "", linehl = "", numhl = "" })
 vim.fn.sign_define("DapBreakpointCondition", { text = "󰓁", texthl = "", linehl = "", numhl = "" })
