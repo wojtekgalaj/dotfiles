f8009e91e2c36e4f08a1f09dea3199838427f803
Wojtek <wojtek.galaj@hey.com>
2025-12-21 12:40:22 +0100

    remove provider from opencode

--- nvim/.config/nvim/lua/custom/plugins/opencode.lua
+++ nvim/.config/nvim/lua/custom/plugins/opencode.lua
@@ -7,12 +7,6 @@ return {
     },
     config = function()
       ---@type opencode.Opts
-      vim.g.opencode_opts = {
-        -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition".
-        provider = {
-          enabled = "tmux",
-        },
-      }
 
       -- Required for `opts.events.reload`.
       vim.o.autoread = true
