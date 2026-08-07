70c6c5e1bf87972a9f7eb2f32db2c3249db79aea
Wojtek Galaj <wojtek@Wojteks-MacBook-Pro.local>
2026-08-07 23:39:07 +0200

    nvim: treesitter and conform fixes

--- nvim/.config/nvim/lua/custom/conform.lua
+++ nvim/.config/nvim/lua/custom/conform.lua
@@ -3,27 +3,24 @@ local conform = require "conform"
 -- This method checks for if a config file for your formatter exists before
 -- returning the formatters table or nil
 ---@return table
-local use_these_if_prettierrc_found = function()
-  local prettierrc_exists = vim.fn.findfile(".prettierrc", ".;")
-  local prettierrc_json_exists = vim.fn.findfile(".prettierrc.json", ".;")
-  local prettierrc_js_exists = vim.fn.findfile(".prettierrc.js", ".;")
-  if prettierrc_exists ~= "" or prettierrc_json_exists ~= "" or prettierrc_js_exists ~= "" then
-    return { "prettierd" }
-  else
-    return {}
-  end
-end
 
 conform.setup {
   formatters_by_ft = {
     lua = { "stylua" },
-    typescript = use_these_if_prettierrc_found(),
-    typescriptreact = use_these_if_prettierrc_found(),
-    javascript = use_these_if_prettierrc_found(),
-    svelte = use_these_if_prettierrc_found(),
-    json = use_these_if_prettierrc_found(),
-    markdown = use_these_if_prettierrc_found(),
+    typescript = { "prettierd" },
+    typescriptreact = { "prettierd" },
+    javascript = { "prettierd" },
+    svelte = { "prettierd" },
+    json = { "prettierd" },
+    markdown = { "prettierd" },
   },
+
+  formatters = {
+    prettierd = {
+      require_cwd = true,
+    },
+  },
+
   format_on_save = {
     lsp_format = "never",
     quiet = true,
